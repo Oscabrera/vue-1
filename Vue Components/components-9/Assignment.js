c24c84736de13ca7005abc7507b2d40a6f1679ec
Oscar Cabrera <o.cabrera@cyberpuerta.mx>
2023-03-30 09:09:21 -0600

    Episode 9: Parent-Child State Communication

---
+++ Vue Components/components-9/Assignment.js
@@ -0,0 +1,12 @@
+export default {
+    props:{
+        assignment: Object
+    },
+    template:`
+    <li>
+        <label class="p-2 flex justify-between items-center">{{assignment.name}}
+        <input type="checkbox" v-model="assignment.complete" class="ml-3">
+        </label>
+    </li>
+    `
+}
